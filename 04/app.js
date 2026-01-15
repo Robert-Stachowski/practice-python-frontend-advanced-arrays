1f75ea70f049ba74f168b04195b06ddc2605a5b6
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-15 16:42:21 +0100

    Map years array to years passed since given dates

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,14 @@
 const years = [1980, 1934, 2002, 2019];
+
+
+const now = new Date();
+const currentYear = now.getFullYear();
+const mapYears = years.map(parsYearsToData);
+
+
+function parsYearsToData(year) {
+    return currentYear - year
+        
+}
+
+console.log(mapYears);
