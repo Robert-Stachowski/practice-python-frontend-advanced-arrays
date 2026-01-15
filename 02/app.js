d9074262d405ee24de834f7137ec7fc89e4a517e
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-15 10:43:51 +0100

    Iterate over array using for and forEach and display last element

--- 02/app.js
+++ 02/app.js
@@ -11,13 +11,37 @@ console.log(randomArray);
 function createRandomArray() {
     const arr = [];
     const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
+    for (let i = 0; i < len; i++) {
+        arr.push(getRandomInteger(1, 100));
     }
 
     return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
+
+
+
+console.log();
+
+
+
+for (let i = 0; i < randomArray.length; i++) {
+    console.log(randomArray[i]);
+};
+const lastNumberFor = randomArray.length - 1;
+console.log("Ostatni element: ", randomArray[lastNumberFor]);
+
+
+
+console.log();
+
+
+
+randomArray.forEach(function (element, index, randomArray) {
+    console.log(element);
+});
+const lastNumber = randomArray.length - 1;
+console.log("Ostatni element: ", randomArray[lastNumber]);
