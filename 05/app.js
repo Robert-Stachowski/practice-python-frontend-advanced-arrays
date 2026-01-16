0225975940c89c61b5bed5caef0a0beb2d0b93aa
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-16 12:02:00 +0100

    Refactor variable and function names for consistency

--- 05/app.js
+++ 05/app.js
@@ -1,18 +1,18 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-const czyParzysta = numbers.filter(checkNumbers);
+const evenNumbers = numbers.filter(isEven);
 
-function checkNumbers(element){
+function isEven(element){
     return (element % 2 === 0 );
 }
 
-console.log(czyParzysta);
+console.log(evenNumbers);
 
-const sumaParzystych = czyParzysta.reduce(sumFunction,0);
+const sumOfEvenNumbers = evenNumbers.reduce(sumFunction,0);
 
 function sumFunction(accumulator,element){
     return accumulator+element;
     
 }
 
-console.log(sumaParzystych)
+console.log(sumOfEvenNumbers)
