88b3399371ff322adae654a18fcf7aef82c5c64b
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-16 11:59:29 +0100

    Calculate sum of even numbers using filter and reduce

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,18 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const czyParzysta = numbers.filter(checkNumbers);
+
+function checkNumbers(element){
+    return (element % 2 === 0 );
+}
+
+console.log(czyParzysta);
+
+const sumaParzystych = czyParzysta.reduce(sumFunction,0);
+
+function sumFunction(accumulator,element){
+    return accumulator+element;
+    
+}
+
+console.log(sumaParzystych)
